39d9c0dad5580ebf4bb7410ed4cef83fc6219255
Rachael Serur <rachaelse@gmail.com>
2014-05-23 12:11:39 -0400

    Saving koan progress.

---
+++ about_scoring_project.rb
@@ -0,0 +1,128 @@
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+
+# Greed is a dice game where you roll up to five dice to accumulate
+# points.  The following "score" function will be used to calculate the
+# score of a single roll of the dice.
+#
+# A greed roll is scored as follows:
+#
+# * A set of three ones is 1000 points
+#
+# * A set of three numbers (other than ones) is worth 100 times the
+#   number. (e.g. three fives is 500 points).
+#
+# * A one (that is not part of a set of three) is worth 100 points.
+#
+# * A five (that is not part of a set of three) is worth 50 points.
+#
+# * Everything else is worth 0 points.
+#
+#
+# Examples:
+#
+# score([1,1,1,5,1]) => 1150 points
+# score([2,3,4,6,2]) => 0 points
+# score([3,4,5,3,3]) => 350 points
+# score([1,5,1,2,4]) => 250 points
+#
+# More scoring examples are given in the tests below:
+#
+# Your goal is to write the score method.
+
+def score(dice)
+
+  score = 0
+  ones = 0
+  nums = dice.uniq
+
+  # count ones
+  dice.each do |die|
+    if die == 1
+      ones +=1
+    end
+  end
+
+  # 3 ones = 1000
+  if ones == 3
+    score += 1000
+
+  # More than 3 ones equals 1000 plus 100 each additional one
+  elsif ones > 3
+    score += 1000
+    ones -= 3
+    score += ones * 100
+  # Less than 3 ones equals 100 * # of ones
+  elsif ones < 3
+    score += ones * 100
+  end
+
+  # Score non-ones
+  nums.each do |num|
+
+    # Variable for frequency of number
+    num_frequency = dice.count(num)
+
+    # If frequency of non-one is 3, add num * 100
+    if num != 1 && num_frequency == 3
+      score += num * 100
+    end
+
+    # If frequency of five is less than 3, add 50 per five
+    if num == 5 && num_frequency < 3
+      score += num_frequency * 50
+
+    # If freuqncy of five is more than 3, add 500 then 50 for each additional 5
+    elsif num == 5 && num_frequency > 3
+      score += num * 100
+      num_frequency -= 3
+      score += num_frequency * 50
+    end
+
+  end
+
+  score
+
+end
+
+class AboutScoringProject < Neo::Koan
+  def test_score_of_an_empty_list_is_zero
+    assert_equal 0, score([])
+  end
+
+  def test_score_of_a_single_roll_of_5_is_50
+    assert_equal 50, score([5])
+  end
+
+  def test_score_of_a_single_roll_of_1_is_100
+    assert_equal 100, score([1])
+  end
+
+  def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
+    assert_equal 300, score([1,5,5,1])
+  end
+
+  def test_score_of_single_2s_3s_4s_and_6s_are_zero
+    assert_equal 0, score([2,3,4,6])
+  end
+
+  def test_score_of_a_triple_1_is_1000
+    assert_equal 1000, score([1,1,1])
+  end
+
+  def test_score_of_other_triples_is_100x
+    assert_equal 200, score([2,2,2])
+    assert_equal 300, score([3,3,3])
+    assert_equal 400, score([4,4,4])
+    assert_equal 500, score([5,5,5])
+    assert_equal 600, score([6,6,6])
+  end
+
+  def test_score_of_mixed_is_sum
+    assert_equal 250, score([2,5,2,2,3])
+    assert_equal 550, score([5,5,5,5])
+    assert_equal 1100, score([1,1,1,1])
+    assert_equal 1200, score([1,1,1,1,1])
+    assert_equal 1150, score([1,1,1,5,1])
+  end
+
+end
